0dcda4e1cefc613096884a05210e535e7bf177e7
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-16 07:13:05 -0300

    Refactor page imports to use render functions

--- PetDor/pages/__init__.py
+++ PetDor/pages/__init__.py
@@ -1,22 +1,20 @@
 """
 Módulo de páginas da aplicação PET DOR
 """
-from pages import (
-    login,
-    cadastro,
-    recuperar_senha,
-    reset_senha,
-    avaliacao,
-    historico,
-    conta
-)
+from pages.login import render_login_page
+from pages.cadastro import render_cadastro_page
+from pages.recuperar_senha import render_recuperar_senha_page
+from pages.reset_senha import render_reset_senha_page
+from pages.avaliacao import render_avaliacao_page
+from pages.historico import render_historico_page
+from pages.conta import render_conta_page
 
 __all__ = [
-    'login',
-    'cadastro',
-    'recuperar_senha',
-    'reset_senha',
-    'avaliacao',
-    'historico',
-    'conta'
+    'render_login_page',
+    'render_cadastro_page',
+    'render_recuperar_senha_page',
+    'render_reset_senha_page',
+    'render_avaliacao_page',
+    'render_historico_page',
+    'render_conta_page'
 ]
