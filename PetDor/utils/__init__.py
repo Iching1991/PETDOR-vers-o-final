5d15ce968a6d25d1fe243ffb49f8322e75bbfd63
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-16 07:44:49 -0300

    Update __init__.py

--- PetDor/utils/__init__.py
+++ PetDor/utils/__init__.py
@@ -1,11 +1,12 @@
 """
-Módulo utilitário do PETDor
+Módulo utilitário do PETDor.
 
-Este pacote contém funções auxiliares usadas em todo o sistema,
-como validações, envio de e-mails e geração de PDFs.
+Contém funções auxiliares como:
+- validações,
+- envio de e-mails,
+- geração de PDFs.
 """
 
-# Exposição organizada dos módulos do pacote utils
 from . import validators
 from . import email_sender
 from . import pdf_generator
