910d39501df7723deaa86e6124654acb98944a1c
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-17 15:11:36 -0300

    Update init.py

--- PetDor/database/init.py
+++ PetDor/database/init.py
@@ -1,13 +1,10 @@
 """
 Módulo de inicialização do pacote database do PETDOR.
 
-Expõe os módulos connection, migration e models para fácil acesso.
+Expõe módulos para importação.
 """
 
-from . import connection
-from . import migration
+from .connection import conectar_db
+from .migration import criar_tabelas
 
-connection.conectar_db()
-migration.criar_tabelas()
-
-__all__ = ["connection", "migration", "models"]
+__all__ = ["conectar_db", "criar_tabelas"]
