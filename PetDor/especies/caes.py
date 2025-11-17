4781ebf399b9accc63ce2c4a542baebf926949b9
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-16 23:11:03 -0300

    Update caes.py

--- PetDor/especies/caes.py
+++ PetDor/especies/caes.py
@@ -14,26 +14,21 @@ CONFIG_CAES = EspecieConfig(
         Pergunta(texto="Meu cão pouca energia", invertida=True),
         Pergunta(texto="Meu cão foi brincalhão", invertida=False),
         Pergunta(texto="Meu cão fez as suas atividades favoritas", invertida=False),
-
         # Alimentação
         Pergunta(texto="O apetite do meu cão reduziu", invertida=True),
         Pergunta(texto="Meu cão comeu normalmente a sua comida favorita", invertida=False),
-
         # Mobilidade
         Pergunta(texto="Meu cão reluta para levantar", invertida=True),
         Pergunta(texto="Meu cão teve problemas para levantar-se ou deitar-se", invertida=True),
         Pergunta(texto="Meu cão teve problemas para caminhar", invertida=True),
         Pergunta(texto="Meu cão caiu ou perdeu o equilíbrio", invertida=True),
-
         # Comportamento Social
         Pergunta(texto="Meu cão gosta de estar perto de mim", invertida=False),
         Pergunta(texto="Meu cão mostrou uma quantidade normal de afeto", invertida=False),
         Pergunta(texto="Meu cão gostou de ser tocado ou acariciado", invertida=False),
-
         # Comportamento Geral
         Pergunta(texto="Meu cão agiu normalmente", invertida=False),
         Pergunta(texto="Meu cão teve problemas para ficar confortável", invertida=True),
-
         # Sono
         Pergunta(texto="Meu cão dormiu bem durante a noite", invertida=False),
     ]
