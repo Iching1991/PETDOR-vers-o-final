4599ee3445c5f7833225925bed8b318d0f7a433f
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-16 21:14:43 -0300

    Update __init__.py

--- PetDor/utils/__init__.py
+++ PetDor/utils/__init__.py
@@ -8,12 +8,6 @@ Responsável por:
 - funções auxiliares.
 """
 
-# Não importe os submódulos aqui para evitar conflitos
-# Os arquivos que precisarem devem importar diretamente:
-# from utils.validators import validar_email
-# from utils.email_sender import enviar_email_reset
-# from utils.pdf_generator import gerar_relatorio_pdf
-
 __all__ = [
     "validators",
     "email_sender",
