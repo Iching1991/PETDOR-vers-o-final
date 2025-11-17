2878d68f7be6aba0da987db0c8b5eddeae6d6646
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-16 22:42:20 -0300

    Update init.py

--- PetDor/database/init.py
+++ PetDor/database/init.py
@@ -1,32 +1,19 @@
 """
-Módulo de gerenciamento de banco de dados do PETDor.
+Módulo de banco de dados do PETDor.
 
 Responsável por:
 - conexão com SQLite,
 - inicialização de tabelas,
-- operações CRUD de usuários e avaliações.
-"""
-
-__all__ = [
-    "connection",
-    "models",
-]
-"""
-Módulo de banco de dados do PETDor.
-
-Inclui:
-- inicialização do banco,
-- criação de conexões,
+- migrações,
 - modelos de dados.
 """
 
 from . import connection
-from . import init
+from . import migration
 from . import models
 
 __all__ = [
     "connection",
-    "init",
+    "migration",
     "models",
 ]
-
